84af654365d7e2af32f4edc1e866310593fabaf7
Daniil <75831226+Raziel536@users.noreply.github.com>
2020-12-22 18:46:06 +0500

    Update encryption.c

--- encryption.c
+++ encryption.c
@@ -6,20 +6,21 @@
 
 void mutableCaesar(char* string, const int offset) {
     bool flag = false;
-    int offset_edited = offset % 26;
-    int i, k = strlen(string);
+    int offsetEdited = offset % 26;
+    int i;
+    const int k = strlen(string);
     for (i = 0; i < k; ++i) {
         if(string[i] >= 'A' && string[i] <= 'Z') {
             string[i] += 'a' - 'A';
             flag = true;
         }
-        if(string[i] + offset_edited > 'z') {
+        if(string[i] + offsetEdited > 'z') {
             string[i] -= ('z' - 'a') + 1;
         }
-        if(string[i] + offset_edited < 'a') {
+        if(string[i] + offsetEdited < 'a') {
             string[i] += ('z' - 'a') + 1;
         }
-        string[i] += offset_edited;
+        string[i] += offsetEdited;
         if(flag) {
             string[i] -= 'a' - 'A';
             flag = false;
@@ -28,51 +29,35 @@ void mutableCaesar(char* string, const int offset) {
 }
 
 
-char* immutableCaesar(char* string, const int offset) {
-    bool flag = false;
-    int offset_edited = offset % 26;
-    int i, k = strlen(string);
-    char* new_string = malloc (sizeof(char) * (k + 1));
-    strcpy(new_string, string);
-    for (i = 0; i < k; ++i) {
-        if(new_string[i] >= 'A' && new_string[i] <= 'Z') {
-            new_string[i] += 'a' - 'A';
-            flag = true;
-        }
-        if(new_string[i] + offset_edited > 'z') {
-            new_string[i] -= ('z' - 'a') + 1;
-        }
-        if(new_string[i] + offset_edited < 'a') {
-            new_string[i] += ('z' - 'a') + 1;
-        }
-        new_string[i] += offset_edited;
-        if(flag) {
-            new_string[i] -= 'a' - 'A';
-            flag = false;
-        }
+char* immutableCaesar(char* string, const int offset) {   
+    const int k = strlen(string);
+    char* newString = (char*) malloc((k + 1) * sizeof(char));
+    if (newString == NULL) {
+        printf("Error - out of memory\n");
     }
-    return new_string;
+    strcpy(newString, string);
+    mutableCaesar(newString, offset);
+    return newString;
 }
 
 
 void mutableXOR (char* string, const char* key) {
     int i;
-    int k = strlen(string);
-    int key_len = strlen(key);
+    const int k = strlen(string);
+    const int keyLen = strlen(key);
     for (i = 0; i < k; ++i) {
-        string[i] ^= key[i % key_len];
+        string[i] ^= key[i % keyLen];
     }
 }
 
 
 char* immutableXOR (char* string, const char* key) {
-    int i;
-    int k = strlen(string);
-    int key_len = strlen(key);
-    char* new_string = malloc (sizeof(char) * (k + 1));
-    strcpy(new_string, string);
-    for (i = 0; i < k; ++i) {
-        new_string[i] ^= key[i % key_len];
+    const int k = strlen(string);
+    char* newString = (char *) malloc (sizeof(char) * (k + 1));
+    if (newString == NULL) {
+        printf("Error - out of memory\n");
     }
-    return new_string;
+    strcpy(newString, string);
+    mutableXOR(newString, key);
+    return newString;
 }
